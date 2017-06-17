50a408fe79db8237e4b5fcaa17a93f322ea4f6cb
gbaik <gideonbaik@gmail.com>
2017-06-16 20:45:07 -0700

    Add GET request from user input to Github API

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,25 +9,68 @@ class App extends React.Component {
     super(props);
     this.state = { 
       repos: []
-    }
+    };
+    // this.staticData = [];
+    // this.search = this.search.bind(this);
+  }
+
+  // search (data) {
+  //   // this.handleAddRepo(data);
+  //   //   , ((item) => {
+  //   //   // this.staticData.push(item);
+  //   //   console.log(item);
+  //   // }));
+
+  //   this.github(() => {
+  //     console.log(item);
+  //   })
+  // }
+
+  getRepo(data) {
 
   }
 
-  search (data) {
-    // console.log(`${term} was searched`);
-    console.log(data);
-    // $.ajax({
-    //   url: '/repos/import',
-    //   method: 'POST',
-    //   dataType: 'JSON',
-    //   data: data,
-    //   success: function() {
-    //     console.log('YES');
-    //   },
-    //   error: function() {
-    //     console.log('NO');
-    //   }
-    // });
+  receiveNewUser(data) {
+    this.getUserRepo(data, (item) => {
+      // this.handleAddRepo(item);
+      console.log(item);
+      this.setState({repos: item});
+    })
+  }
+
+  getUserRepo(data, callback) {
+    var url = 'https://api.github.com/users/' + data +'/repos';
+
+    $.ajax({
+      url: url,
+      type: 'GET',
+      contentType: 'application/JSON',
+      success: function(data) {
+        console.log('SUCCESSFUL GET');
+        callback(data);
+      },
+      error: function() {
+        console.log('GET error');
+      }
+    });
+  }
+
+  handleAddRepo (data) {
+    // console.log(data);
+    var username = {"user": data};
+    $.ajax({
+      url: '/repos/import',
+      type: 'POST',
+      data: JSON.stringify(username),
+      contentType: 'application/JSON',
+      success: function() {
+        console.log('SUCCESSFUL POST');
+        callback()
+      },
+      error: function() {
+        console.log('POST error');
+      }
+    });
   }
 
   render () {
@@ -35,7 +78,7 @@ class App extends React.Component {
     <div>
       <h1>Github Fetcher</h1>
       <RepoList repos={this.state.repos}/>
-      <Search onSearch={this.search.bind(this)}/>
+      <Search onSearch={this.receiveNewUser.bind(this)}/>
     </div>)
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,16 +1,33 @@
+// var db = require('../database/index;')
 var express = require('express');
-
 var app = express();
+
+var staticData = 'gbaik';
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.post('/repos/import', function (req, res) {
-  // TODO
+app.get('https://api.github.com/users/gbaik/repos', function(request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('hello');
 });
 
-app.get('/repos', function (req, res) {
-  // TODO
+app.post('/repos/import', function (request, response) {
+  var body = '';
+  request.on('data', function(chunk) {
+    body += chunk;
+  }).on('end', function() {
+    response.writeHead(201);
+    body = JSON.parse(body);
+    response.end();
+  }).on('error', function() {
+    response.end();
+  })
 });
+
+// app.get('/repos', function (request, response) {
+//   console.log('a')
+//   response.send(response.data);
+// });
 
 var port = 1128;
 
